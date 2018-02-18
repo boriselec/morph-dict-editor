6dc38eb9d69b211e2634bc1e6a20f2b29abb7e8c
boris puzikov <boris.puzikov@gmail.com>
2018-02-18 12:39:08 +0300

    empty content tag workaround: replaceAll
    There is no way to collapse tags via XmlEventWriter.
    Standalone tag is not supported.

--- src/main/java/com/boriselec/morphdict/App.java
+++ src/main/java/com/boriselec/morphdict/App.java
@@ -2,7 +2,10 @@ package com.boriselec.morphdict;
 
 import com.boriselec.morphdict.edit.XmlTransformer;
 
-import javax.xml.stream.*;
+import javax.xml.stream.XMLEventFactory;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
 import java.io.*;
 
 public class App {
@@ -14,11 +17,32 @@ public class App {
         XmlTransformer xmlTransformer = new XmlTransformer(xmlFactory, outFactory, xmlEventFactory);
 
         InputStream inputStream = new FileInputStream("C:\\Users\\boris\\Downloads\\dict.opcorpora.xml\\dict.opcorpora.xml");
-        OutputStream outputStream = new FileOutputStream("C:\\Users\\boris\\Downloads\\dict.opcorpora.xml\\dict.opcorpora.filtered.xml");
+        OutputStream outputStream = new FileOutputStream("C:\\Users\\boris\\Downloads\\dict.opcorpora.xml\\dict.opcorpora.filtered.tmp.xml");
 
         xmlTransformer.transform(inputStream, outputStream);
 
         inputStream.close();
         outputStream.close();
+
+        postProcess();
+    }
+
+    private static void postProcess() throws IOException {
+        PrintWriter writer = new PrintWriter("C:\\Users\\boris\\Downloads\\dict.opcorpora.xml\\dict.opcorpora.filtered.xml", "UTF-8");
+
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\boris\\Downloads\\dict.opcorpora.xml\\dict.opcorpora.filtered.tmp.xml"));
+        String line = br.readLine();
+
+        while (line != null) {
+            writer.println(line
+                .replaceAll("></g>", "/>")
+                .replaceAll("></link>", "/>")
+                .replaceAll("utf-8\"\\?>", "utf-8\" standalone=\"yes\"?>\n")
+            );
+            line = br.readLine();
+        }
+
+        br.close();
+        writer.close();
     }
 }
