482293a24434d698fd39ed112b45af5efe935af4
boris puzikov <boris.puzikov@gmail.com>
2018-02-24 16:51:14 +0300

    json output support

--- src/main/java/com/boriselec/morphdict/App.java
+++ src/main/java/com/boriselec/morphdict/App.java
@@ -1,6 +1,9 @@
 package com.boriselec.morphdict;
 
 import com.boriselec.morphdict.edit.*;
+import de.odysseus.staxon.json.JsonXMLConfig;
+import de.odysseus.staxon.json.JsonXMLConfigBuilder;
+import de.odysseus.staxon.json.JsonXMLOutputFactory;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.xml.stream.*;
@@ -11,8 +14,14 @@ public class App {
         System.setProperty("line.separator", "\n");
 
         XMLInputFactory xmlFactory = XMLInputFactory.newInstance();
-        XMLOutputFactory outFactory = XMLOutputFactory.newInstance();
+        XMLOutputFactory xmlOutputFactory = JsonXMLOutputFactory.newFactory();
         XMLEventFactory eventFactory = XMLEventFactory.newFactory();
+
+        JsonXMLConfig config = new JsonXMLConfigBuilder()
+            .autoArray(true)
+            .prettyPrint(true)
+            .build();
+        XMLOutputFactory jsonOutputFactory = new JsonXMLOutputFactory(config);
 
         LemmaHandler handler = new ChainLemmaHandler(
             new BlackListTextLemmaFilter("ёж"),
@@ -22,7 +31,7 @@ public class App {
         EventFilter xmlTagFilter = new XmlTagFilter("grammemes", "link_types", "links", "restrictions");
         XmlAttributeFilter xmlAttrFilter = new XmlAttributeFilter(eventFactory, "id", "rev");
 
-        XmlTransformer xmlTransformer = new XmlTransformer(xmlFactory, outFactory, handler, xmlTagFilter, xmlAttrFilter);
+        XmlTransformer xmlTransformer = new XmlTransformer(xmlFactory, xmlOutputFactory, handler, xmlTagFilter, xmlAttrFilter);
 
         InputStream inputStream = new FileInputStream("C:\\Users\\boris\\Downloads\\dict.opcorpora.xml\\dict.opcorpora.xml");
         OutputStream outputStream = new FileOutputStream("C:\\Users\\boris\\Downloads\\dict.opcorpora.xml\\dict.opcorpora.filtered.tmp.xml");
